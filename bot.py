28043fd790d0c8923dba3f6373817fa2078ceb33
darkowl618 <121595462+darkowl618@users.noreply.github.com>
2023-11-07 22:57:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ AuthChat = filters.chat(CHANNELS) if CHANNELS else (filters.group | filters.chan
 User     = Client(name = "AcceptUser", session_string = environ.get("SESSION"))
 
 
-@User.on_message(filters.command(["run", "approve", "start"], [".", "/"]) & AuthChat)                     
+@User.on_message(filters.command(["run", "hai", "decline", "start"], [".", "/"]) & AuthChat)                     
 async def approve(client: User, message: Message):
     Id = message.chat.id
     await message.delete(True)
@@ -20,24 +20,24 @@ async def approve(client: User, message: Message):
     try:
        while True: # create loop is better techniq 🙃
            try:
-               await client.approve_all_chat_join_requests(Id)         
+               await client.decline_all_chat_join_requests(Id)         
            except FloodWait as t:
                asyncio.sleep(t.value)
-               await client.approve_all_chat_join_requests(Id) 
+               await client.decline_all_chat_join_requests(Id) 
            except Exception as e:
                logging.error(str(e))
     except FloodWait as s:
         asyncio.sleep(s.value)
         while True:
            try:
-               await client.approve_all_chat_join_requests(Id)         
+               await client.decline_all_chat_join_requests(Id)         
            except FloodWait as t:
                asyncio.sleep(t.value)
-               await client.approve_all_chat_join_requests(Id) 
+               await client.decline_all_chat_join_requests(Id) 
            except Exception as e:
                logging.error(str(e))
 
-    msg = await client.send_message(Id, "**Task Completed** ✓ **Approved Pending All Join Request**")
+    msg = await client.send_message(Id, "**Task Completed** ✓")
     await asyncio.sleep(3)
     await msg.delete()
 
